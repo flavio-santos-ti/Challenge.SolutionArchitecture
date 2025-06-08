3ad49086dd34d70d57618211b0d6a8b4b8fa001f
unknown <flavio.santos.ti@outlook.com>
2025-06-08 14:45:46 -0300

    ✨feat(launching-service): implementar criação de transação e consulta por data.

--- src/Challenge.SolutionArchitecture.LaunchingService/Models/Transaction.cs
+++ src/Challenge.SolutionArchitecture.LaunchingService/Models/Transaction.cs
@@ -3,7 +3,7 @@
 public class Transaction
 {
     public Guid Id { get; set; }
-    public DateTime OccurredAt { get; set; }
+    public DateOnly OccurredAt { get; set; }
     public decimal Amount { get; set; }
     public string Type { get; set; } = null!; // "credit" ou "debit"
     public string? Description { get; set; }
--- src/Challenge.SolutionArchitecture.LaunchingService/Repositories/ITransactionRepository.cs
+++ src/Challenge.SolutionArchitecture.LaunchingService/Repositories/ITransactionRepository.cs
@@ -5,5 +5,5 @@ namespace Challenge.SolutionArchitecture.LaunchingService.Repositories;
 public interface ITransactionRepository
 {
     Task AddAsync(Transaction transacion);
-    Task<Transaction?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Transaction>> GetByDateAsync(DateOnly date);
 }
